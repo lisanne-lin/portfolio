134a80a4bd693518c4bdc9e4bd6f13ff41998b6b
Lisanne Lin <lisanne.lin@hva.nl>
2023-04-22 23:14:08 +0200

    changed name to property for og tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,18 +13,24 @@ export default defineNuxtConfig({
 			title: "Lisanne Lin | Portfolio",
 			meta: [
 				{ charset: "utf-8" },
+
 				{
 					name: "viewport",
 					content: "width=device-width, initial-scale=1",
 				},
 				{
-					hid: "image",
-					name: "image",
-					content: "/assets/img/og-image.png",
+					hid: "og:title",
+					property: "og:title",
+					content: "Lisanne Lin | Portfolio",
 				},
 				{
-					hid: "description",
-					name: "description",
+					hid: "og:image",
+					property: "og:image",
+					content: "/og-image.png",
+				},
+				{
+					hid: "og:description",
+					property: "og:description",
 					content:
 						"Hi! My name is Lisanne Lin and welcome to my portfolio!",
 				},
